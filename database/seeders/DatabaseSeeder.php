080b98a2b88b8a0191a3ee3e4c8cd212eebfa81e
faizalyagami <faizalyagami@gmail.com>
2025-01-21 14:49:17 +0700

    fixed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,21 @@ class DatabaseSeeder extends Seeder
         \App\Models\Petugas::create([
             'nama_petugas'  => 'Administrator',
             'username'  => 'admin',
-            'telp' => '08174882661',
+            'telp' => '628174882661',
             'password'  => bcrypt('12345678'),
             'roles' => 'admin'
+        ]);
+
+        Mahasiswa::create([
+            'npm' => '10050024000',
+            'name' => 'Faisal',
+            'email' => 'faizalyagami@gmail.com',
+            'email_verified_at' => now(),
+            'username' => 'faisalabdillah',
+            'jenis_kelamin' => 'Laki-laki',
+            'password' => bcrypt('password'),
+            'telp' => '628174882661',
+            'alamat' => 'Bandung'
         ]);
     }
 }
--- database/seeders/PetugasSeeder.php
+++ database/seeders/PetugasSeeder.php
@@ -19,85 +19,92 @@ class PetugasSeeder extends Seeder
             [
                 'nama_petugas'  => 'Dr. Dewi Sartika, M.Si',
                 'username'  => 'D940198',
-                'telp' => '08122021514',
+                'telp' => '628122021514',
                 'password'  => bcrypt('D940198'),
                 'roles' => 'petugas',
             ],
             [
                 'nama_petugas'  => 'Ali Mubarak, S.Psi., M.Psi.',
                 'username'  => 'D070465',
-                'telp' => '081221820024',
+                'telp' => '6281221820024',
                 'password'  => bcrypt('D070465'),
                 'roles' => 'petugas',
             ],
             [
                 'nama_petugas'  => 'Dr. Lilim Halimah, BHSc., MHSPY.',
                 'username'  => 'D040388',
-                'telp' => '087821295500',
+                'telp' => '6287821295500',
                 'password'  => bcrypt('D040388'),
                 'roles' => 'petugas',
             ],
             [
                 'nama_petugas'  => 'Suhana, S.Psi., M.Psi.',
                 'username'  => 'D000329',
-                'telp' => '081910066125',
+                'telp' => '6281910066125',
                 'password'  => bcrypt('D000329'),
                 'roles' => 'petugas',
             ],
             [
                 'nama_petugas'  => 'Dr. Siti Qodariah, M.Psi.',
                 'username'  => 'D900112',
-                'telp' => '0818228119',
+                'telp' => '62818228119',
                 'password'  => bcrypt('D900112'),
                 'roles' => 'petugas',
             ],
             [
                 'nama_petugas'  => 'Farida Coralia, S.Psi., M.Psi.',
                 'username'  => 'D080470',
-                'telp' => '087824050920',
+                'telp' => '6287824050920',
                 'password'  => bcrypt('D080470'),
                 'roles' => 'petugas',
             ],
             [
                 'nama_petugas'  => 'Dr. Eneng Nurlaili Wangi, M.Psi.',
                 'username'  => 'D970266',
-                'telp' => '087822180071',
+                'telp' => '6287822180071',
                 'password'  => bcrypt('D970266'),
                 'roles' => 'petugas',
             ],
             [
                 'nama_petugas'  => 'Dr. Endah Nawangsih, Dra., M.Psi.',
                 'username'  => 'D970265',
-                'telp' => '087821971110',
+                'telp' => '6287821971110',
                 'password'  => bcrypt('D970265'),
                 'roles' => 'petugas',
             ],
             [
                 'nama_petugas'  => 'Rizka Hadian Permana, S.Psi., M.Psi.',
                 'username'  => 'D150673',
-                'telp' => '085294862549',
+                'telp' => '6285294862549',
                 'password'  => bcrypt('D150673'),
                 'roles' => 'petugas',
             ],
             [
                 'nama_petugas'  => 'Nida Hamidah, S.Psi',
                 'username'  => 'A150337',
-                'telp' => '081320200982',
+                'telp' => '6281320200982',
                 'password'  => bcrypt('A150337'),
                 'roles' => 'petugas',
             ],
             [
                 'nama_petugas'  => 'Nurmariam',
                 'username'  => 'A910111',
-                'telp' => '082216418470',
+                'telp' => '6282216418470',
                 'password'  => bcrypt('A910111'),
                 'roles' => 'petugas',
             ],
             [
                 'nama_petugas'  => 'Undang Kamaludin',
                 'username'  => 'A020217',
-                'telp' => '081322370437',
+                'telp' => '6281322370437',
                 'password'  => bcrypt('A020217'),
+                'roles' => 'petugas',
+            ],
+            [
+                'nama_petugas'  => 'Noer Fitriani, S.Psi',
+                'username'  => 'A180405',
+                'telp' => '6282187785000',
+                'password'  => bcrypt('A180405'),
                 'roles' => 'petugas',
             ]
         ];
@@ -105,10 +112,44 @@ class PetugasSeeder extends Seeder
         // Insert petugas data
         Petugas::insert($petugasData);
 
-        $aliMubarak = Petugas::where('username', 'D070465')->first();
-        $aliMubarak->struktural()->attach([2, 3]);
+        // Menghubungkan Petugas dengan id_struktural sesuai data
+        $petugas = Petugas::where('username', 'D940198')->first();
+        $petugas->struktural()->attach(1);  // Dr. Dewi Sartika, M.Si
 
-        $nidaHamidah = Petugas::where('username', 'A150337')->first();
-        $nidaHamidah->struktural()->attach([2, 3]);
+        $petugas = Petugas::where('username', 'D070465')->first();
+        $petugas->struktural()->attach([2, 3]);  // Ali Mubarak, S.Psi., M.Psi.
+
+        $petugas = Petugas::where('username', 'D040388')->first();
+        $petugas->struktural()->attach(4);  // Dr. Lilim Halimah, BHSc., MHSPY.
+
+        $petugas = Petugas::where('username', 'D000329')->first();
+        $petugas->struktural()->attach(5);  // Suhana, S.Psi., M.Psi.
+
+        $petugas = Petugas::where('username', 'D900112')->first();
+        $petugas->struktural()->attach(2);  // Dr. Siti Qodariah, M.Psi.
+
+        $petugas = Petugas::where('username', 'D080470')->first();
+        $petugas->struktural()->attach(2);  // Farida Coralia, S.Psi., M.Psi.
+
+        $petugas = Petugas::where('username', 'D970266')->first();
+        $petugas->struktural()->attach(3);  // Dr. Eneng Nurlaili Wangi, M.Psi.
+
+        $petugas = Petugas::where('username', 'D970265')->first();
+        $petugas->struktural()->attach(3);  // Dr. Endah Nawangsih, Dra., M.Psi.
+
+        $petugas = Petugas::where('username', 'D150673')->first();
+        $petugas->struktural()->attach(6);  // Rizka Hadian Permana, S.Psi., M.Psi.
+
+        $petugas = Petugas::where('username', 'A150337')->first();
+        $petugas->struktural()->attach([2, 3]);  // Nida Hamidah, S.Psi
+
+        $petugas = Petugas::where('username', 'A910111')->first();
+        $petugas->struktural()->attach(4);  // Nurmariam
+
+        $petugas = Petugas::where('username', 'A020217')->first();
+        $petugas->struktural()->attach(5);  // Undang Kamaludin
+
+        $petugas = Petugas::where('username', 'A180405')->first();
+        $petugas->struktural()->attach(6);  // Noer Fitriani
     }
 }

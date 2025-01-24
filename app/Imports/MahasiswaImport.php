5144517f103252a52454c8cf751e4512b5e1d941
faizalyagami <faizalyagami@gmail.com>
2025-01-24 10:44:01 +0700

    set limit 300

--- app/Imports/MahasiswaImport.php
+++ app/Imports/MahasiswaImport.php
@@ -14,6 +14,12 @@ class MahasiswaImport implements ToModel, WithHeadingRow
      *
      * @return \Illuminate\Database\Eloquent\Model|null
      */
+
+    // public function __construct()
+    // {
+    //     set_time_limit(300);
+    // }
+
     public function model(array $row)
     {
         return new Mahasiswa([

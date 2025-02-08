6ba1243803ebb140e25932109233b111e3ef6ab9
faizalyagami <faizalyagami@gmail.com>
2025-02-08 17:16:24 +0700

    try error keluhan

--- app/Http/Controllers/Admin/KeluhanController.php
+++ app/Http/Controllers/Admin/KeluhanController.php
@@ -22,7 +22,7 @@ class KeluhanController extends Controller
         // dd($loggedPetugas);
 
         $keluhan = Keluhan::with(['kategori', 'evaluasi'])
-            ->where('statusd', $status)
+            ->where('status', $status)
             // ->when($loggedPetugas->roles == 'petugas', function ($q) use ($loggedPetugas) {
             //     if (in_array($loggedPetugas->nama_petugas, [
             //         'Dr. Dewi Sartika, M.Si'
